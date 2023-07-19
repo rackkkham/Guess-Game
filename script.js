fa7e5ee85005e1c3db88dfa4dbd488f79859cdae
Rackham <garikamirkhanyan@yahoo.com>
2023-07-20 00:18:08 +0400

    Optimized code and improve logic

--- script.js
+++ script.js
@@ -24,25 +24,14 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
-    // When guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    // When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game';
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    score--, (document.querySelector('.score').textContent = score);
+    guess < secretNumber
+      ? (document.querySelector('.message').textContent = '📉 Too low')
+      : (document.querySelector('.message').textContent = '📈 Too High');
+    if (score < 1) {
+      document.querySelector('.message').textContent = '💥 You Lost the Game';
       document.querySelector('.score').textContent = 0;
     }
   }
